f8272433ebd34cd03c60aa9ad80e3fe05467140f
deepakdcsa <deepak.kumar@dcsa.org>
2021-11-24 17:47:38 +0100

    callback notification testing

---
+++ src/main/resources/org/dcsa/api/validator/steps/NotificationWebhookSteps.java
@@ -0,0 +1,8 @@
+package org.dcsa.api.validator.steps;
+
+public class NotificationWebhookSteps {
+    public NotificationWebhookSteps() {
+        Given("^Callback end point$", () -> {
+        });
+    }
+}
