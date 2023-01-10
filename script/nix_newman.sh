d73dfd16995314785b914f9bffcc5b8028d82624
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2023-01-10 17:03:20 +0100

    Dockerfile update for AWS

--- script/nix_newman.sh
+++ script/nix_newman.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-newman run ./provider-ctk-local.postman-collection.json
+is_running_in_container() {
+  awk -F: '/cpuset/ && $3 ~ /^\/$/ { c=1 } END{ exit c }' /proc/self/cgroup
+}
+
+if is_running_in_container; then
+  echo "Aye!! I'm in a container"
+  newman run ./provider-ctk-local-docker.postman-collection.json
+else
+  echo "Nay!! I'm not in a container"
+  newman run ./provider-ctk-local-docker.postman-collection.json
+fi
