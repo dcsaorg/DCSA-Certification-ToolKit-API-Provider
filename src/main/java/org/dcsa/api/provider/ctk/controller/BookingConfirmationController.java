7eb4797d28f7795120e038c5aa16f64f5bf952fd
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2023-05-23 15:46:21 +0200

    Implement booking id controller

--- src/main/java/org/dcsa/api/provider/ctk/controller/BookingConfirmationController.java
+++ src/main/java/org/dcsa/api/provider/ctk/controller/BookingConfirmationController.java
@@ -15,7 +15,9 @@ import java.util.concurrent.Executors;
 
 @Controller
 public class BookingConfirmationController {
-	private CompletableFuture<String> completableFuture;
+
+	private String officialBookingId;
+/*	private CompletableFuture<String> completableFuture;
 	public CompletableFuture<String> getBookingIdAsync(String bookingId) {
 		completableFuture = new CompletableFuture<>();
 		Executors.newCachedThreadPool().submit(() -> {
@@ -24,45 +26,45 @@ public class BookingConfirmationController {
 			return null;
 		});
 		return completableFuture;
-	}
+	}*/
 
-	@PostMapping("/form")
+	@PostMapping("/officialBookingId")
 	@ResponseBody
 	public ResponseEntity<String> submitForm(@RequestParam("bookingId") String bookingId) throws ExecutionException, InterruptedException {
 		System.out.println("Received bookingId: " + bookingId);
-		completableFuture = getBookingIdAsync(bookingId);
-		return ResponseEntity.ok("Booking id is set successfully "+completableFuture.get());
+		officialBookingId = bookingId;
+		return ResponseEntity.ok("Booking id:" +officialBookingId+ "is set successfully ");
 	}
 	@GetMapping("/bookingView")
-	public String  getData(Model model) throws ExecutionException, InterruptedException {
-		if (completableFuture == null) {
-			model.addAttribute("errorMessage1", "Booking ID is not submitted by official API yet! Refresh the page after the booking id is set by the official API.");
-			model.addAttribute("errorMessage2", "Newman will set the booking ID when it runs the official booking API.");
+	public String  getVide(Model model) {
+		if (officialBookingId == null) {
+			model.addAttribute("errorMessage1", "Booking ID is not submitted by official API yet!");
+			model.addAttribute("errorMessage2", "Please wait Newman will set the booking ID when it runs the official booking API. It will show the booking ID.");
 			return "errorView";
 		}
-		// Wait for the form to be completed
-		var bookingId = completableFuture.get();
-		model.addAttribute("id", bookingId);
-		int delayAmount = 3000; // Delay amount in milliseconds
+		model.addAttribute("id", officialBookingId);
+		int delayAmount = AppProperty.BOOKING_DELAY; // Delay amount in milliseconds
 		model.addAttribute("delayAmount", delayAmount);
 		return "bookingView";
 	}
 
 	@GetMapping("/bookingId")
 	@ResponseBody
-	public String getBookingId() throws ExecutionException, InterruptedException {
-		if(completableFuture == null){
+	public String getBookingId() {
+		if(officialBookingId == null){
 			return  "Booking ID is not submitted by official API yet!";
 		}else{
-			return completableFuture.get();
+			return officialBookingId;
 		}
 	}
 
-	@GetMapping("/example")
-	public String example() throws InterruptedException {
-		// Simulate delay on the server-side
+	@PostMapping("/submitForm")
+	@ResponseBody
+	public ResponseEntity<String> submittedBookingId(@RequestParam("id") String id) throws InterruptedException {
+		System.out.println("Submitted ID: " + id);
 		Thread.sleep(AppProperty.BOOKING_DELAY);
-		return "Delayed response after " + AppProperty.BOOKING_DELAY + " milliseconds";
+		return ResponseEntity.ok("Delayed response after configured " + AppProperty.BOOKING_DELAY + " milliseconds. Booking ID submitted: " + id);
 	}
+
 
 }
